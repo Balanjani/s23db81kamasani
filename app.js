b46d0328af9dc5a7b88b14c563d5a2169b683762
Balanjani kamasani <40666589+Balanjani@users.noreply.github.com>
2023-11-28 19:46:46 -0600

    updated files

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var watchesRouter = require('./routes/watches');
@@ -29,6 +51,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -37,12 +67,21 @@ app.use('/watches', watchesRouter);
 app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/watches.js
+++ routes/watches.js
@@ -1,6 +1,14 @@
 var express = require('express');
 const watches_controllers= require('../controllers/watches');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
+
 /* GET watches */
 router.get('/', watches_controllers.watches_view_all_Page);
 
@@ -14,9 +22,16 @@ router.get('/detail', watches_controllers.watches_view_one_Page);
 /* GET create costume page */
 router.get('/create', watches_controllers.watches_create_Page);
 
-/* GET create update page */
-router.get('/update', watches_controllers.watches_update_Page);
-
 /* GET delete watches page */
 router.get('/delete', watches_controllers.watches_delete_Page);
+
+/* GET update watches page */
+//router.get('/update', watches_controllers.watches_update_Page);
+
+
+router.get('/update', secured, watches_controllers.watches_update_Page);
+
+
+
+
 
